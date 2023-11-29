e80e833867a240b06482b423e6d305fbb7589c70
Dorothée L. Berthold <152390074+DorotheeBerthold@users.noreply.github.com>
2023-11-29 15:56:30 +0100

    Create data import.R

---
+++ R/data import.R
@@ -0,0 +1,9 @@
+############################################################
+## Metabolomic & 16S integration for correlation analysis ##
+## Dorothée L. Berthold, ETH Zürich                       ##
+############################################################
+
+#Import required packages and functions
+source("gutPackages.R")
+gutPackages()
+
