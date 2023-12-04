08da92a1e378136d724b6ed92eb7ca00eb886409
Dorothée L. Berthold <152390074+DorotheeBerthold@users.noreply.github.com>
2023-12-04 10:51:31 +0100

    Update stacked_barplot.R

--- R/stacked_barplot.R
+++ R/stacked_barplot.R
@@ -1,12 +1,9 @@
-#calculate mean abundance across biological replicates
-microbes_long_mean <- microbes_long  %>%  
-  group_by(Bacteria, site, origin) %>% 
-  mutate(mean_abundance = mean(Abundance, na.rm = T)) %>% 
-  ungroup() %>%  
-  group_by(mean_abundance) %>%  
-  distinct(Bacteria, site, origin)
+############################################################
+## Metabolomic & 16S integration for correlation analysis ##
+## Dorothée L. Berthold, ETH Zürich                       ##
+############################################################
 
-microbes_long_mean <- as.data.frame(microbes_long_mean)
+#use microbes_long_mean generated in 1_dataimport.R #######
 
 #factor the categorical variables
 microbes_long_mean$site <- factor(microbes_long_mean$site, levels = c("duo", "jej", "ile", "cec", "col"))
