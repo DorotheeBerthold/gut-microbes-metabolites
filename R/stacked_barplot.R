d4c69a815f960274543cbdc8ee411333cbc82fb3
Dorothée L. Berthold <152390074+DorotheeBerthold@users.noreply.github.com>
2023-12-04 08:07:01 +0100

    Create stacked_barplot.R

---
+++ R/stacked_barplot.R
@@ -0,0 +1,20 @@
+#calculate mean abundance across biological replicates
+microbes_long_mean <- microbes_long |> 
+  group_by(Bacteria, site, origin) |> 
+  mutate(mean_abundance = mean(Abundance, na.rm = T)) |> 
+  ungroup() |> 
+  group_by(mean_abundance) |> 
+  distinct(Bacteria, site, origin)
+
+microbes_long_mean <- as.data.frame(microbes_long_mean)
+
+#factor the categorical variables
+microbes_long_mean$site <- factor(microbes_long_mean$site, levels = c("duo", "jej", "ile", "cec", "col"))
+
+ggplot(microbes_long_mean, aes(y = mean_abundance, x = site, fill = Bacteria)) +
+  geom_bar(position = "stack", stat = "identity") +
+  theme_classic() +
+  labs(x = "", y = "Relative abundance (%)") +
+  ggtitle("Relative abundances in the lumen of 12w male mice") +
+  facet_wrap(~origin, scales = "free", ncol = 2) +
+  scale_fill_brewer(palette = "RdYlBu")
